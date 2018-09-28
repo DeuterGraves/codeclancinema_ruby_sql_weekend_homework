90a97b40eda9168011de005b031efbe13b3e2b8b
DeuterGraves <puckles@gmail.com>
2018-09-28 11:45:38 +0100

    film class basic set up, film can be created, but not yet saved

--- cinema_controller.rb
+++ cinema_controller.rb
@@ -1,10 +1,29 @@
 require_relative("./models/customer.rb")
+require_relative("./models/film.rb")
 require("pry")
+
+# delete customers
+# delete films
+# delete tickets
+
+# build and save customers
 
 customer1 = Customer.new({
   "name" => "Rebecca Davenport",
   "funds" => 50
   })
+
+  # build and save films
+film1 = Film.new({
+  "title" => "Charlie and the Chocolate Factory",
+  "price" => 2
+  })
+
+
+
+  # set up and save tickets
+
+
 
 
   binding.pry
